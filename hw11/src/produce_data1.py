7d34bd76e1e6ebc30678b5c3cfa99eb832dad4bf
zhehui-chen <62113801+zhehui-chen@users.noreply.github.com>
2021-05-11 10:59:35 +0800

    Add files via upload

--- hw11/src/produce_data1.py
+++ hw11/src/produce_data1.py
@@ -12,29 +12,24 @@ for t in range(0,46,1):
     receve_list.append(d)
     time_stamp.append(t)
 
-## 製造數據
 for i in range(0,46,1):
     receve_list[i] = round(num_list[i] + np.random.normal(0, 20),5)
 print(num_list)
 print(receve_list)
 
-# 開啟 CSV 檔案
-with open('/home/ee405423/Desktop/output.csv', 'w', newline='') as csvfile:
-# 建立 CSV 檔寫入器
+with open('/home/zhe_hui/Desktop/output.csv', 'w') as csvfile:
   writer = csv.writer(csvfile)
-  # 寫入一列資料
   writer.writerow(num_list)
   writer.writerow(receve_list)
 
 
-
 plt.figure(figsize=(6,8))
-plt.subplot(411)
+plt.subplot(211)
 plt.title('The Original Signal')
 plt.plot(time_stamp[0:46],num_list[0:46])
 
+plt.subplot(212)
 plt.title('Noise Signal')
-plt.subplot(412)
 plt.plot(time_stamp[0:46],receve_list[0:46])
 
 plt.show()
